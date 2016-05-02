fe45a63643ee0178dc645890a9963eb11d438e85
peper11111 <peper.pleban@gmail.com>
2016-05-02 16:32:21 +0200

    Zmieniony uklad strony. Dodane animacje po scrollu. Dodane miejsce na stopke

--- ProjektZespolowy/src/main/web/scripts/script.js
+++ ProjektZespolowy/src/main/web/scripts/script.js
@@ -10,29 +10,81 @@ function selectActiveTab(num) {
 }
 
 $(document).ready(function () {
-    var boxHeight = $(".background-color").height();
     var headerHeight = $(".masthead").height();
 
+    /* Animate page on click of tab */
     $(".scroll").click(function () {
         var n = $(this).index();
-        $('html, body').stop().animate({scrollTop: n * boxHeight - headerHeight}, 500);
+        var offset = $(".content-block:nth-child(" + (n + 2) + ")").offset();
+        $('html, body').stop().animate({scrollTop: offset.top - headerHeight}, 500);
     });
 
-    $(window).scroll(function () {
-        var position = $(this).scrollTop();
-        if (position < 440) {
-            selectActiveTab(1);
-        } else if (position < 940) {
+    /* Autoselect active tab */
+    $(".content-block:nth-child(2)").waypoint(function () {
+        selectActiveTab(1);
+    }, {offset: "0"});
+    $(".content-block:nth-child(3)").waypoint(function (direction) {
+        if (direction == "down")
             selectActiveTab(2);
-        } else if (position < 1440) {
+        else if (direction == "up")
+            selectActiveTab(1);
+    }, {offset: "60px"});
+    $(".content-block:nth-child(4)").waypoint(function (direction) {
+        if (direction == "down")
             selectActiveTab(3);
-        } else if (position < 1940) {
+        else if (direction == "up")
+            selectActiveTab(2);
+    }, {offset: "60px"});
+    $(".content-block:nth-child(5)").waypoint(function (direction) {
+        if (direction == "down")
             selectActiveTab(4);
+        else if (direction == "up")
+            selectActiveTab(3);
+    }, {offset: "60px"});
+    $(".content-block:nth-child(6)").waypoint(function (direction) {
+        if (direction == "down")
+            selectActiveTab(5);
+        else if (direction == "up")
+            selectActiveTab(4);
+    }, {offset: "61px"});
 
-        }
-    });
+    /* Animations */
+    $(".section1 h1").addClass("fadeInLeft");
+    $(".section1 h3").addClass("fadeInRight");
 
-    /* Animations on load */
-    $(".title-box h1").fadeTo(500, 1).animate({left: 20}, 700);
-    $(".title-box h3").fadeTo(400, 1).animate({right: 50}, 700);
+    $("#tile01").waypoint(function () {
+        $("#tile01").addClass("fadeInUp");
+        setTimeout(function () {
+            $("#tile02").addClass("fadeInUp");
+        }, 100);
+        setTimeout(function () {
+            $("#tile03").addClass("fadeInUp");
+        }, 200);
+    }, {offset: "90%"});
+
+    $(".comment:nth-child(1)").waypoint(function () {
+        $(".comment:nth-child(1)").addClass("fadeInLeft")
+    }, {offset: "90%"});
+    $(".comment:nth-child(2)").waypoint(function () {
+        $(".comment:nth-child(2)").addClass("fadeInRight")
+    }, {offset: "90%"});
+    $(".comment:nth-child(3)").waypoint(function () {
+        $(".comment:nth-child(3)").addClass("fadeInLeft")
+    }, {offset: "90%"});
+
+    $(".section5 h1").waypoint(function () {
+        $(".section5 h1").addClass("fadeInRight")
+    }, {offset: "90%"});
+    $(".section5 h3:nth-child(2)").waypoint(function () {
+        $(".section5 h3:nth-child(2)").addClass("fadeInLeft")
+    }, {offset: "90%"});
+    $(".section5 h3:nth-child(3)").waypoint(function () {
+        $(".section5 h3:nth-child(3)").addClass("fadeInRight")
+    }, {offset: "90%"});
+    $(".section5 h3:nth-child(4)").waypoint(function () {
+        $(".section5 h3:nth-child(4)").addClass("fadeInLeft")
+    }, {offset: "90%"});
+    $(".section5 button").waypoint(function () {
+        $(".section5 button").addClass("flipInX")
+    }, {offset: "90%"});
 });
