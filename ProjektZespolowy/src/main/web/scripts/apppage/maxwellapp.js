a93e27cb3e8e6cc5d1b66ad2bd3e726e72c1ca7d
matem1 <chewas15@gmail.com>
2016-06-10 14:50:38 +0200

    Refactor Kalkulatorów,Historia Treningów, Atlas, Poprawa UI Kalkulatorów, Treningu, Atlas,

--- ProjektZespolowy/src/main/web/scripts/apppage/maxwellapp.js
+++ ProjektZespolowy/src/main/web/scripts/apppage/maxwellapp.js
@@ -1,12 +1,4 @@
 var App = angular.module('maxApp',['ui.router','ngResource']);
-    var countries = {
-    "India": "2",
-    "England": "2",
-    "Brazil": "3",
-    "UK": "1",
-    "USA": "3",
-    "Syria": "2"
-    };
 
     App.config(function($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise("/menu");
@@ -42,111 +34,96 @@ var App = angular.module('maxApp',['ui.router','ngResource']);
 
 
 
-    App.controller('calculatorController', function(){
-        this.chosen = 1;
-        this.weight = 0;//centymetry
-        this.height = 0;//kilogramy
-        this.bmi = 0;
-        this.water = 0;
-        this.whr = 0;
-        this.comment = "";
-        this.waist = 0;
-        this.hip = 0;
-        this.sex = "";
-        this.idealW = 0;
-        this.proteins = 0;
-        this.activity = "";
+    App.controller('calculatorController', function($scope){
 
+        $scope.chosen = 1;
 
-        this.choseCalc = function(chosen){
-            this.chosen = chosen;
+        $scope.choseCalc = function(chosen){
+            $scope.chosen = chosen;
         };
 
-        this.isChosen = function(chosen){
-            return (chosen === this.chosen);
+        $scope.isChosen = function(chosen){
+            return (chosen === $scope.chosen);
         };
 
-        this.checkBMI = function(value){
+        $scope.checkBMI = function(value){
             if(value <= 18.5){
-                this.comment = "Underweight";
+                $scope.comment = "Underweight";
             }
             else if(value <= 25){
-                this.comment = "Normal healthy weight";
+                $scope.comment = "Normal healthy weight";
             }
             else if(value <= 27){
-                this.comment = "Overweight at risk";
+                $scope.comment = "Overweight at risk";
             }
             else if(value <= 30){
-                this.comment = "Overweight - moderately obese";
+                $scope.comment = "Overweight - moderately obese";
             }
             else if(value > 30){
-                this.comment = "Overweight - severely obese";
+                $scope.comment = "Overweight - severely obese";
             }
         };
 
-        this.checkWHR = function(value ,sex ){
+        $scope.checkWHR = function(value ,sex ){
           if(sex ==='m'){
               if(value <= 0.9){
-                  this.comment = "Normal weight";
+                  $scope.comment = "Normal weight";
               }else if (value <= 0.99){
-                  this.comment = "Over-weight";
+                  $scope.comment = "Over-weight";
               }else if(value >1.00){
-                  this.comment = "Obesity";
+                  $scppe.comment = "Obesity";
               }
           }else if(sex ==='w'){
               if(value <= 0.8){
-                  this.comment = "Normal weight";
+                  $scope.comment = "Normal weight";
               }else if (value <= 0.84){
-                  this.comment = "Over-weight";
+                  $scope.comment = "Over-weight";
               }else if(value >0.85){
-                  this.comment = "Obesity";
+                  $scope.comment = "Obesity";
               }
           }
         };
 
-        this.calcBMI = function(height, weight){
+        $scope.calcBMI = function(height, weight){
             height = parseFloat(height/100);
-            this.checkBMI(parseFloat(weight/(height*height)));
-            this.bmi = parseFloat(weight/(height*height));
-            this.weight = 0;
-            this.height = 0;
+            $scope.bmi = parseFloat(weight/(height*height));
+            $scope.checkBMI($scope.bmi);
         };
 
-        this.calcWater = function(weight) {
-            if (weight <= 10) {
-                this.water = weight * 100;
-            }else if(weight <= 20){
-                this.water = 10*100 + (weight-10)*50;
+        $scope.calcWater = function(wweight) {
+            if (wweight <= 10) {
+                $scope.water = wweight * 100;
+            }else if(wweight <= 20){
+                $scope.water = 10*100 + (wweight-10)*50;
             }else{
-                this.water = 10*100 + 10*50 + (weight-20)*20;
+                $scope.water = 10*100 + 10*50 + (wweight-20)*20;
             }
-            this.weight = 0;
         };
 
-        this.calcWHR = function(waist, hip, sex) {
-            this.whr = waist/hip;
-            this.checkWHR(this.whr, sex);
+        $scope.calcWHR = function(waist, hip, sex) {
+            $scope.whr = waist/hip;
+            $scope.checkWHR($scope.whr, sex);
         };
 
-        this.calcIdeal = function(height, sex){
+        $scope.calcIdeal = function(iheight, sex){
             if(sex === "m"){
-                this.idealW = height - 100 - ((height - 150)/4);
+                $scope.idealW = iheight - 100 - ((iheight - 150)/4);
             }else if( sex === "w"){
-                this.idealW = height - 100 - ((height - 150)/2);
+                $scope.idealW = iheight - 100 - ((iheight - 150)/2);
             }
         };
 
-        this.calcDailyCalor = function(){
+        $scope.calcDailyCalor = function(){
 
         };
 
-        this.calcRecomProt = function(activity, weight){
+        $scope.calcRecomProt = function(activity, pweight){
             if(activity ==='1'){
-                this.proteins = weight*0.8;
+                $scope.proteins = pweight*0.8;
             }else if(activity === '2'){
-                this.proteins = weight;
+                $scope.proteins = pweight;
             }else if(activity === '3'){
-                this.proteins = weight*1.5;
+                $scope.proteins = pweight*1.5;
             }
         };
 
@@ -160,10 +137,10 @@ var App = angular.module('maxApp',['ui.router','ngResource']);
 
     App.controller('trainingController', function($scope, TrainingData){
         $scope.items = TrainingData.query({user:1});
-        //$scope.getTrainingInfo = function(trainID){
-        //    $scope.selected= items[trainID];
-        //    console.log(selected);
-        //}
+        $scope.warunek = true;
+        $scope.getTrainingInfo = function(trainID){
+            $scope.selected= $scope.items[trainID];
+        }
 
     });
 
@@ -178,11 +155,11 @@ var App = angular.module('maxApp',['ui.router','ngResource']);
 
         $scope.getBodyPart = function(bodyId){
             $scope.exercises = ExerciseData.query({bodyPart: bodyId});
-        }
+        };
 
-        $scope.getExerciseDetail = function(exerID){
-            $scope.currentExercise = ExerciseData.get({exercise: exerID});
-        }
+        $scope.getExerciseDetail = function(number){
+            $scope.currentExercise = $scope.exercises[number];
+        };
 
     });
 
@@ -195,9 +172,5 @@ var App = angular.module('maxApp',['ui.router','ngResource']);
 
         $scope.stats = StatsData.get({part: 'users',id: '1'});
 
-
-        //$scope.getExerciseDetail = function(exerID){
-        //    $scope.currentExercise = ExerciseData.get({exercise: exerID});
-        //}
 
     });
